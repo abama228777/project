ccf2b0ac0074b74ad11534ac3698832bab945057
abama228777 <kozlovvladislavv2011@gmail.com>
2025-05-17 12:28:09 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,32 @@
+import pygame
+from pygame.locals import*
+import random 
+pygame.init()
+
+width = 500
+height = 500
+scree_size = (widht, height)
+screen = pygame.display.set_mode(screen_size)
+pygame.display.set_caption('Car Game')
+
+grey = (100,100,100)
+green = (76, 208, 56)
+red = (200, 0, 0)
+white = (255, 255, 255)
+yellow(255, 232, 0)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
