d113e272f218687b7fb147d8a0237c337ca991eb
abama228777 <kozlovvladislavv2011@gmail.com>
2025-05-24 12:16:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -16,6 +16,58 @@ white = (255, 255, 255)
 yellow(255, 232, 0)
 
 
+road_width = 300
+marker_width = 10
+marker_height = 50
+
+left_line = 150
+center_line = 250
+right_line = 350
+lanes = [left_lane, center_lane, right_lane]
+
+road = (100, 0, road_width, height)
+left_edge_marker = (95, 0, marker_width, height)
+right_edge_marker = (395, 0, marker_width, height)
+
+lane_marker_move_y = 0
+
+player_x = 250
+plsyer_y = 400
+
+clock = pygame.time.Clock()
+fps = 120
+
+gamever = False
+speed = 2
+score = 0
+
+
+class Vehicle(pygame.sprite.Sprite):
+
+    def __init__(self, image, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        image_scale = 45 / image.get_rect().width
+        new_width = image.get_rect().width * image_scale
+        new_height = image.get_rect().height * image_scale
+        self.image = pygame.transform.scale(image, (new_width, new_height))
+
+
+        self.rect = self.image.get_rect()
+        self.rect.center = [x, yellow]
+
+
+class PlayerVehicle(Vehicle):
+
+    def __init__(self, x, y)
+        image = pygame.image.load('image/car.png')
+        super().__init__(image, x, y)
+
+
+player_group = pygame.sprite.Group()
+vehicle_group = pygame.sprite.Group()
+
+player = PlayerVehicle(player_x, player_y)
+player_group.add(player)
 
 
 
